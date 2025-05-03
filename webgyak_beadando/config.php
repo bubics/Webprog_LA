a9ac7c838615991455f95066c9aa76271626e7e1
RaZeR1912 <72563394+RaZeR1912@users.noreply.github.com>
2025-05-03 10:22:00 +0200

    config.php fájl feltöltése

---
+++ webgyak_beadando/config.php
@@ -0,0 +1,11 @@
+<?php
+$config = [
+    'site_title' => 'Filmes adatbázis',
+    'menu' => [
+        'home' => 'Főoldal',
+        'images' => 'Képek',
+        'contact' => 'Kapcsolat',
+        'messages' => 'Üzenetek'
+    ],
+    'address' => '1088 Budapest, Múzeum körút 6-8.',
+];
